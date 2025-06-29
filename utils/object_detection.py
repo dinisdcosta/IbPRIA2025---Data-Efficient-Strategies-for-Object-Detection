83e73c43dd5fd2e2479984d124bc29c62349274e
dinisdcosta <45609008+dinisdcosta@users.noreply.github.com>
2025-06-29 21:06:56 +0100

    Update object_detection.py

--- utils/object_detection.py
+++ utils/object_detection.py
@@ -16,7 +16,7 @@ def train_yolov5(img=1280, epochs=50, data='dataset/data.yaml', name='new_train'
     subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     print(f"Training YOLOv5 completed. Results saved in {project}/train/{name}.")
 
-def detect_yolov5(img=1280, source='imgs/', name='new_detect', project='detect', weights='best.pt'):
+def detect_yolov5(img=1280, source='imgs/', name='new_detect', project='temp', weights='best.pt'):
     cmd = [
         "python", os.path.join('yolov5', 'detect.py'),
         "--save-txt", "--save-conf",
@@ -28,7 +28,7 @@ def detect_yolov5(img=1280, source='imgs/', name='new_detect', project='detect',
     ]
     subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-def test_yolov5(img=1280, data='dataset/data.yaml', name='new_test', project='test', weights="''"):
+def test_yolov5(img=1280, data='dataset/data.yaml', name='new_test', project='temp', weights="''"):
     cmd = [
         "python", os.path.join('yolov5', 'val.py'),
         "--data", f"{data}",
@@ -45,12 +45,12 @@ def train_yolo(img=1280, epochs=50, data='dataset/data.yaml', name='new_train', 
     results = model.train(data=data, epochs=epochs, imgsz=img, name=name, project=project, batch= batch)
     return results
 
-def detect_yolo(img=1280, source='imgs/', name='detect', project='.temp/detect', weights='yolo11n.pt'):
+def detect_yolo(img=1280, source='imgs/', name='detect', project='temp/detect', weights='yolo11n.pt'):
     model = YOLO(weights)
     results = model(source=source, imgsz=img, name=name, project=project, save=True, save_txt=True, save_conf=True)
     return results
 
-def test_yolo(img=1280, data='dataset/data.yaml', name='test', project='.temp/test', weights="yolo11n.pt"):
+def test_yolo(img=1280, data='dataset/data.yaml', name='test', project='temp/test', weights="yolo11n.pt"):
     model = YOLO(weights)
     results = model.val(data=data, imgsz=img, name=name, project=project, split="test", exist_ok=True)
     return results
